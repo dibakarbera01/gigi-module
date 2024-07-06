b413bca7b1da15951f6396806483f151bd443591
Dibakar <dibakarbera01@gmail.com>
2024-07-06 16:43:33 +0530

    feat(utils): :zap: gigi website home page
    gigi website home page design using wrappid
    
    Ref: #1

--- app/routes.registry.js
+++ app/routes.registry.js
@@ -1,4 +1,12 @@
+
+import Home from "./components/Home";
 export const RoutesRegistry = {
+  Home: {
+    Page        : { appComponent: Home.name },
+    authRequired: false,
+    entityRef   : "home",
+    url         : "home"
+  },
   defaultModuleRoute: {
     Page        : { appComponent: "ModuleComponent" },
     authRequired: false,
